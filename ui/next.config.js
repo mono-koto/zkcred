ac4c48372a11a7495571a4582caa48a051ad40f1
brent <brent@mono-koto.com>
2022-11-29 11:23:33 -0800

    fix base path

--- ui/next.config.js
+++ ui/next.config.js
@@ -38,8 +38,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === "production" ? "/ZKVC" : undefined, // update if your repo name changes for 'npm run deploy' to work successfully
-  assetPrefix: process.env.NODE_ENV === "production" ? "/ZKVC/" : undefined, // update if your repo name changes for 'npm run deploy' to work successfully
+  basePath: process.env.NODE_ENV === "production" ? "/zkcred" : undefined, // update if your repo name changes for 'npm run deploy' to work successfully
+  assetPrefix: process.env.NODE_ENV === "production" ? "/zkcred/" : undefined, // update if your repo name changes for 'npm run deploy' to work successfully
 };
 
 module.exports = nextConfig;
