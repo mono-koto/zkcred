264794803c2aba2045f081759148a97e6cbc41a8
brent <brent@mono-koto.com>
2022-11-29 11:51:09 -0800

    fix coi-serviceworker path

--- ui/pages/reactCOIServiceWorker.tsx
+++ ui/pages/reactCOIServiceWorker.tsx
@@ -6,7 +6,7 @@ function loadCOIServiceWorker() {
     window.location.hostname != "localhost"
   ) {
     const coi = window.document.createElement("script");
-    coi.setAttribute("src", "/coi-serviceworker.min.js"); // update if your repo name changes for npm run deploy to work successfully
+    coi.setAttribute("src", "/zkcred/coi-serviceworker.min.js"); // update if your repo name changes for npm run deploy to work successfully
     window.document.head.appendChild(coi);
   }
 }
